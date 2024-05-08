01a08650e79927e6a32a9bafb582a238781bf4c8
AnnaB <annabenettoni@gmail.com>
2024-05-08 17:45:33 +0200

    devo andare

--- novezzina/src/pages/benvenuti.js
+++ novezzina/src/pages/benvenuti.js
@@ -1,23 +1,31 @@
 import './css/benvenuti.css';
 import BottomGreen from './components/bottomGreen';
-import Buttons from './components/buttons';
 import { ReactComponent as Logo } from "../img/logo.svg";
 import { ReactComponent as Fiorello } from "../img/fiorello.svg";
 
 
 export default function Benvenuti() {
-    return(
-        <div className='body'>
+    return (
+        <div className='body-benvenuti'>
             <div id='logo-benvenuti'>
                 <Logo id='logoSize-benvenuti'></Logo>
             </div>
-            <div id='fiorelloPosition-benvenuti'>
-                <Fiorello></Fiorello>
+            <div id='salvavita-benvenuti'>
+                <div id='fiorelloPosition-benvenuti'>
+                    <div id='titolo-benvenuti'>
+                        <h3 id='scritta'>orto botanico</h3>
+                    </div>
+                    <Fiorello></Fiorello>
+                </div>
+                <div id='bottom-benvenuti'>
+                    <div id='positionInizia-benvenuti'>
+                        <div id='styleInizia-benvenuti'>
+                            <h4 id='fontInizia-benvenuti'>INIZIA</h4>
+                        </div>
+                    </div>
+                    <BottomGreen></BottomGreen>
+                </div>
             </div>
-
-            <Buttons></Buttons>
-
-            <BottomGreen></BottomGreen>
         </div>
     )
 }
