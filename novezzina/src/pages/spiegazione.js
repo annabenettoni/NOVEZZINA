6c56cd80d64f0a4dd511bdb3eb718e9d638b7e3e
Yasmine 3 <yassigiuliani@gmail.com>
2024-05-09 15:20:40 +0200

    bottoni indietro ed avanti

--- novezzina/src/pages/spiegazione.js
+++ novezzina/src/pages/spiegazione.js
@@ -6,7 +6,7 @@ import HappyFiorelloMeta from '../img/happyFiorelloMeta.svg';
 export default function Spiegazione() {
     const testi = [
         "Ciao piccoli esploratori! Io sono Fiorello e sono qui per darvi il benvenuto in questa avventura emozionante! Siete pronti a mettere alla prova le vostre abilità? Ora vi dirò poche e semplici regole da seguire, INIZIAMO!",
-        "L’impresa che vi attende è un viaggio attraverso la natura, un viaggio che vi porterà a scoprire segreti nascosti e meraviglie incredibili. Ora, ascoltate attentamente...",
+        "L'impresa che vi attende è un viaggio attraverso la natura, un viaggio che vi porterà a scoprire segreti nascosti e meraviglie incredibili. Ora, ascoltate attentamente...",
         "Regola numero uno: esplorate con curiosità e rispetto. La natura è la nostra guida, me compreso e il nostro tesoro, quindi trattatela con gentilezza e attenzione mentre procedete.",
         "Regola numero due: seguite le indicazioni. Ogni QR code che scannerizzerete vi porterà a una nuova tappa e a una nuova sfida. Ascoltate attentamente ciò che gli animali vi diranno e seguite i loro suggerimenti con attenzione",
         "Regola numero tre: divertitevi! Questa è un'avventura pensata per farvi sorridere, ridere e imparare. Non abbiate paura di esplorare e lasciatevi sorprendere dalla bellezza che vi circonda.",
@@ -15,7 +15,13 @@ export default function Spiegazione() {
 
     const [indiceTestoCorrente, setIndiceTestoCorrente] = useState(0);
 
-    function cambiaTesto() {
+    
+    function cambiaTestoIndietro() {
+        // Decrementa l'indice del testo corrente
+        setIndiceTestoCorrente((prevIndice) => prevIndice - 1);
+    }
+
+    function cambiaTestoAvanti() {
         // Incrementa l'indice del testo corrente
         setIndiceTestoCorrente((prevIndice) => prevIndice + 1);
     }
@@ -27,9 +33,20 @@ export default function Spiegazione() {
                 testo={testi[indiceTestoCorrente]}
                 id="testo"
             />
+                        <div
+                id='styleAvanti-spiegazione-indietro'
+                onClick={cambiaTestoIndietro}
+                style={{
+                    cursor: 'pointer',
+                    width: "45%", // Adatta la larghezza al contenuto
+                }}
+            >
+                <h4 id='fontAvanti-spiegazione'>INDIETRO</h4>
+            </div>
+
             <div
-                id='styleAvanti-spiegazione'
-                onClick={cambiaTesto}
+                id='styleAvanti-spiegazione-avanti'
+                onClick={cambiaTestoAvanti}
                 style={{
                     cursor: 'pointer',
                     width: "45%", // Adatta la larghezza al contenuto
