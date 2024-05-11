27c020136d244440f9ef5c15ed5b134d4aa34591
macs-massimo-piazza <me@massimopiazza.com>
2024-05-11 09:29:47 +0200

    loader for fine page

--- novezzina/src/pages/fine.js
+++ novezzina/src/pages/fine.js
@@ -1,8 +1,13 @@
 import './css/fine.css';
+import FineImg from '../img/fine.svg';
 
 export default function Fine() {
     return (
-        <div className='body-fine'>
-        </div>
+        <>
+            <div className='body-fine'>
+                <img src={FineImg} alt='congratulazioni'/>
+            </div>
+            <div className='loader'></div>
+        </>
     )
 }
